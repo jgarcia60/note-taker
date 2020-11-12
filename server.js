99e2572e228658f43771c0695e95fe414c79eb30
jgarcia60 <68722324+jgarcia60@users.noreply.github.com>
2020-11-12 16:10:43 -0500

    added get routes

--- server.js
+++ server.js
@@ -1,2 +1,35 @@
 
 const express = require('express');
+const path = require("path");
+const fs = require('fs');
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log("listening on localhost:" + PORT);
+});
+
+
+const notes = [];
+
+
+app.get("*", function(req, res) {
+    // res.send("This works");
+    res.sendFile(path.join(__dirname, "/public/index.html"));
+});
+
+app.get("/notes", function(req, res) {
+    // res.send("This works");
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+
+app.get("/api/notes", function(req, res) {
+    // res.send("This works");
+    res.json({
+        notes: notes,
+    })
+});
